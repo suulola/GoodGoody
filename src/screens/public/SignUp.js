c9a8598db2248fad9bd6da493707c8e528379de3
mysuulola <oluwaseyisuulola@gmail.com>
2019-07-27 03:37:20 +0100

    initial commit

---
+++ src/screens/public/SignUp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native'
+
+export class SignUp extends Component {
+  render() {
+    return (
+      <View>
+        <Text> sigup page </Text>
+      </View>
+    )
+  }
+}
+
+export default SignUp
